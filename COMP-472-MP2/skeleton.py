c387b0172b600cf49a1f3c7a651b4d9d3750183b
AbdulSir <abd.sirawan@hotmail.com>
2021-11-17 23:19:08 -0500

    Refactoring code

--- COMP-472-MP2/skeleton.py
+++ COMP-472-MP2/skeleton.py
@@ -13,9 +13,10 @@ class Game:
 	gp = Game_Parameter()
 	d1 = gp.max_depth_d1
 	d2 = gp.max_depth_d2
-	current_depth_max = 0 #the current depth of the recursion
-	current_depth_min = 0
+	# current_depth_max = 0 #the current depth of the recursion
+	# current_depth_min = 0
 	nb_of_evaluated_states = 0
+	current_depth = 0
 	
 	def __init__(self, recommend = True):
 		self.initialize_game()
@@ -67,154 +68,43 @@ class Game:
 				col_index += 1
 		return formatted_list
 
-		# Possible Horizontal Wins
-	# def 
-	#     for i in range(self.gp.size_of_board):
-	#         for j in range(self.gp.size_of_board):
-	#             if (j <= self.gp.size_of_board - self.gp.line_up_size):
-	#                 for k in range(self.gp.line_up_size):
-	#                     if (self.current_state[i][j + k] == 'X' or self.current_state[i][j + k] == '.'):
-	#                         horizontal_X_flag += 1
-	#                     elif (self.current_state[i][j + k] == 'O' or self.current_state[i][j + k] == '.'):
-	#                         horizontal_O_flag += 1
-	#                 self.nb_of_evaluated_states += 1
-	#                 if (self.gp.line_up_size == horizontal_X_flag):
-	#                     horizontal_X_score += 1
-	#                 elif (self.gp.line_up_size == horizontal_O_flag):
-	#                     horizontal_O_score += 1
-	#                 horizontal_X_flag = horizontal_O_flag = 0
-	#     horizontal_score = horizontal_O_score - horizontal_X_score
-
-	# 	# Diagonal win
-	# 	diagonal_X_flag = diagonal_O_flag = diagonal_1_X_flag = diagonal_1_O_flag = 0
-	# 	for i in range(self.gp.size_of_board):
-	# 		for j in range(self.gp.size_of_board):
-	# 			if (i <= self.gp.size_of_board - self.gp.line_up_size and j <= self.gp.size_of_board - self.gp.line_up_size):
-	# 				for k in range(self.gp.line_up_size):
-	# 					if (self.current_state[i+k][j+k] == 'X'):
-	# 						diagonal_X_flag += 1
-	# 					elif (self.current_state[i+k][j+k] == 'O'):
-	# 						diagonal_O_flag += 1
-	# 				if diagonal_X_flag == self.gp.line_up_size:
-	# 					return 'X'
-	# 				elif diagonal_O_flag == self.gp.line_up_size:
-	# 					return 'O'
-	# 				diagonal_X_flag = diagonal_O_flag = 0
-				
-	# 			if (i <= self.gp.size_of_board - self.gp.line_up_size and j >= self.gp.line_up_size -1):
-	# 				for k in range(self.gp.line_up_size):
-	# 					if (self.current_state[i+k][j-k] == 'X'):
-	# 						diagonal_1_X_flag += 1
-	# 					elif (self.current_state[i+k][j-k] == 'O'):
-	# 						diagonal_1_O_flag += 1
-	# 				if diagonal_1_X_flag == self.gp.line_up_size:
-	# 					return 'X'
-	# 				elif diagonal_1_O_flag == self.gp.line_up_size:
-	# 					return 'O'
-	# 				diagonal_1_X_flag = diagonal_1_O_flag = 0	
-
-	#     # Diagonal win
-	#     for i in range(self.gp.size_of_board):
-	#         for j in range(self.gp.size_of_board):
-	#             if (
-	#                     i <= self.gp.size_of_board - self.gp.line_up_size and j <= self.gp.size_of_board - self.gp.line_up_size):
-	#                 for k in range(self.gp.line_up_size):
-	#                     if (self.current_state[i + k][j + k] == 'X' or self.current_state[i + k][j + k] == '.'):
-	#                         diagonal_X_flag += 1
-	#                     elif (self.current_state[i + k][j + k] == 'O' or self.current_state[i + k][j + k] == '.'):
-	#                         diagonal_O_flag += 1
-	#                 self.nb_of_evaluated_states += 1
-	#                 if diagonal_X_flag == self.gp.line_up_size:
-	#                     diagonal_X_score += 1
-	#                 elif diagonal_O_flag == self.gp.line_up_size:
-	#                     diagonal_O_score += 1
-	#                 diagonal_X_flag = diagonal_O_flag = 0
-
-	#             if (i <= self.gp.size_of_board - self.gp.line_up_size and j >= self.gp.line_up_size - 1):
-	#                 for k in range(self.gp.line_up_size):
-	#                     if (self.current_state[i + k][j - k] == 'X' or self.current_state[i + k][j - k] == '.'):
-	#                         diagonal_1_X_flag += 1
-	#                     elif (self.current_state[i + k][j - k] == 'O' or self.current_state[i + k][j - k] == '.'):
-	#                         diagonal_1_O_flag += 1
-	#                 self.nb_of_evaluated_states += 1
-	#                 if diagonal_1_X_flag == self.gp.line_up_size:
-	#                     diagonal_1_X_score += 1
-	#                 elif diagonal_1_O_flag == self.gp.line_up_size:
-	#                     diagonal_1_O_score += 1
-	#                 diagonal_1_X_flag = diagonal_1_O_flag = 0
-	#     diagonal_score = (diagonal_O_score + diagonal_1_O_score) - (diagonal_X_score + diagonal_1_X_score)
-
-	#     # win_paths_for_O - win_paths_for_X
-	#     score = horizontal_score + vertical_score + diagonal_score
-	#     return score
-
-	# Didn't touch this yet
-	# def minimax(self, max=False):
-	# 	# Minimizing for 'X' and maximizing for 'O'
-	# 	# Possible values are:
-	# 	# -1 - win for 'X'
-	# 	# 0  - a tie
-	# 	# 1  - loss for 'X'
-	# 	# We're initially setting it to 2 or -2 as worse than the worst case:
-	# 	x = y = None
-	# 	end = time.time()
-	# 	if (max and (end - self.start) > self.gp.threshold):
-	# 		print('Here')
-	# 		return (-1, None, None)
-	# 	elif max == False and (end - self.start) > self.gp.threshold:
-	# 		print('There')
-	# 		return (1, None, None)
-
-	# 	value = 100000
-	# 	if max:
-	# 		self.current_depth_max += 1
-	# 		value = -100000
-	# 	else:
-	# 		self.current_depth_min += 1
-
 	def is_end(self):
+		horizontal_X_flag = horizontal_O_flag = vertical_X_flag = vertical_O_flag = 0
 		# Vertical win
 		for i in range(self.gp.size_of_board):
-			vertical_X_flag = []
-			vertical_O_flag = []
 			for j in range(self.gp.size_of_board):
-				if (self.current_state[j][i] != '.' and
-						self.current_state[j][i] != '*' and
-						self.current_state[j][i] != 'O'):
-					vertical_X_flag.append(self.current_state[j][i])
-					if len(vertical_X_flag) == self.gp.line_up_size:
+				if (i <= self.gp.size_of_board - self.gp.line_up_size):
+					for k in range(self.gp.line_up_size):
+						if (self.current_state[i+k][j] == 'X'):
+							vertical_X_flag += 1
+						elif (self.current_state[i+k][j] == 'O'):
+							vertical_O_flag += 1
+					if (self.gp.line_up_size == vertical_X_flag):
 						return 'X'
-				elif (self.current_state[j][i] != '.' and
-					  self.current_state[j][i] != '*' and
-					  self.current_state[j][i] != 'X'):
-					vertical_O_flag.append(self.current_state[j][i])
-					if len(vertical_O_flag) == self.gp.line_up_size:
+					elif (self.gp.line_up_size == vertical_O_flag):
 						return 'O'
+					vertical_X_flag = vertical_O_flag = 0
 
 		# Horizontal win
 		for i in range(self.gp.size_of_board):
-			horizontal_X_flag = []
-			horizontal_O_flag = []
 			for j in range(self.gp.size_of_board):
-				if (self.current_state[i][j] != '.' and
-						self.current_state[i][j] != '*' and
-						self.current_state[i][j] != 'O'):
-					horizontal_X_flag.append(self.current_state[j][i])
-					if len(horizontal_X_flag) == self.gp.line_up_size:
+				if (j <= self.gp.size_of_board - self.gp.line_up_size):
+					for k in range(self.gp.line_up_size):
+						if (self.current_state[i][j+k] == 'X'):
+							horizontal_X_flag += 1
+						elif (self.current_state[i][j+k] == 'O'):
+							horizontal_O_flag += 1
+					if (self.gp.line_up_size == horizontal_X_flag):
 						return 'X'
-				elif (self.current_state[i][j] != '.' and
-					  self.current_state[i][j] != '*' and
-					  self.current_state[i][j] != 'X'):
-					horizontal_O_flag.append(self.current_state[j][i])
-					if len(horizontal_O_flag) == self.gp.line_up_size:
+					elif (self.gp.line_up_size == horizontal_O_flag):
 						return 'O'
+					horizontal_X_flag = horizontal_O_flag = 0
 
 		# Diagonal win
 		diagonal_X_flag = diagonal_O_flag = diagonal_1_X_flag = diagonal_1_O_flag = 0
 		for i in range(self.gp.size_of_board):
 			for j in range(self.gp.size_of_board):
-				if (
-						i <= self.gp.size_of_board - self.gp.line_up_size and j <= self.gp.size_of_board - self.gp.line_up_size):
+				if (i <= self.gp.size_of_board - self.gp.line_up_size and j <= self.gp.size_of_board - self.gp.line_up_size):
 					for k in range(self.gp.line_up_size):
 						if (self.current_state[i + k][j + k] == 'X'):
 							diagonal_X_flag += 1
@@ -350,7 +240,6 @@ class Game:
 						diagonal_1_O_score += 1
 					diagonal_1_X_flag = diagonal_1_O_flag = 0
 		diagonal_score = (diagonal_O_score + diagonal_1_O_score) - (diagonal_X_score + diagonal_1_X_score)
-
 		# win_paths_for_O - win_paths_for_X
 		score = horizontal_score + vertical_score + diagonal_score
 		return score
@@ -364,44 +253,61 @@ class Game:
 		# 0  - a tie
 		# 1  - loss for 'X'
 		# We're initially setting it to 2 or -2 as worse than the worst case:
+		if self.player_turn == 'O':
+			depth_limit = self.d2
+		else:
+			depth_limit = self.d1
+
 		x = y = None
 		end = time.time()
 		if max and (end - self.start) > self.gp.threshold:
+			print(self.current_depth)
 			return (-1, None, None)
 		elif max==False and (end - self.start) > self.gp.threshold:
+			print(self.current_depth)
 			return (1, None, None)
-
+		
 		value = 100000
 		if max:
-			self.current_depth_max += 1
 			value = -100000
-		else:
-			self.current_depth_min += 1
 
-		result = self.possible_win_paths()
+		self.current_depth += 1
+
+		end_check = self.is_end()
+		if end_check == 'X':
+			self.current_depth = 0
+			return (-1, x, y)
+		elif end_check == 'O':
+			self.current_depth = 0
+			return (1, x, y)
+		elif end_check == '.':
+			self.current_depth = 0
+			return (0, x, y)
 
 		for i in range(self.gp.size_of_board):
 			for j in range(self.gp.size_of_board):
 				if self.current_state[i][j] == '.':
-					if max and self.current_depth_max <= self.d1:
+					if max and self.current_depth <= depth_limit:
 						self.current_state[i][j] = 'O'
 						(v, _, _) = self.minimax(max=False)
 						if v > value:
 							value = v
 							x = i
 							y = j
-					elif max and self.current_depth_max > self.d1:
-						return(result, i, j)
-					elif max == False and self.current_depth_min <= self.d2:
+					elif max == False and self.current_depth <= depth_limit:
 						self.current_state[i][j] = 'X'
 						(v, _, _) = self.minimax(max=True)
 						if v < value:
 							value = v
 							x = i
 							y = j
-					elif max == False and self.current_depth_min > self.d2:
-						return(result, i, j)
+					#check for the self.current_depth
 					self.current_state[i][j] = '.'
+
+		if self.current_depth > depth_limit:
+			self.current_depth = 0
+			result = self.possible_win_paths()
+			return(result, i, j)
 		return (value, x, y)
 
 	def alphabeta(self, alpha=-2, beta=2, max=False):
@@ -469,6 +375,9 @@ class Game:
 			if self.check_end():
 				return
 			self.start = time.time()
+			self.nb_of_evaluated_states = 0 
+			self.current_depth_max = 0
+			self.current_depth_min = 0
 			if self.gp.minimax_alphabeta_bool == self.MINIMAX:
 				if self.player_turn == 'X':
 					(_, x, y) = self.minimax(max=False)
@@ -492,7 +401,7 @@ class Game:
 						with open(file_name, "a") as myfile:
 							myfile.write(F'Player {self.player_turn} under AI control plays: x = {x}, y = {list_of_chars[y]}\n\n')
 							myfile.write(F'\ni\tEvaluation time: {round(end - self.start, 7)}s\n')
-							myfile.write(F'\nii\tHeuristic evaluations: {self.nb_of_evaluated_states}s\n')
+							myfile.write(F'\nii\tHeuristic evaluations: {self.nb_of_evaluated_states}\n')
 						print(F'Evaluation time: {round(end - self.start, 7)}s')
 						print(F'Player {self.player_turn} under AI control plays: x = {x}, y = {list_of_chars[y]}')
 			self.current_state[x][y] = self.player_turn
